1ac21c32c94f8511f304ed39899f179cd90147a3
GaRyung <shubom0115@naver.com>
2023-11-04 11:02:42 +0900

    # 스프링 기초

---
+++ src/main/java/com/spring/core/chap01/config/HotelManager.java
@@ -0,0 +1,24 @@
+package com.spring.core.chap01.config;
+
+import com.spring.core.chap01.*;
+
+// 호텔을 운영하기 위해 필요한 의존객체를
+// 전담해서 생성해주는 역할
+public class HotelManager {
+
+    // 셰프를 구하는 기능
+    public Chef chef() {
+        return new JannChef();
+    }
+    // 레스토랑을 계약하는 기능
+    public Restaurant restaurant() {
+        return new WesternRestaurant(chef());
+//        WesternRestaurant r = new WesternRestaurant();
+//        r.setChef(chef()); // 수정자(setter) 주입
+//        return r;
+    }
+    // 호텔을 생성하는 기능
+    public Hotel hotel() {
+        return new Hotel(restaurant(), chef());
+    }
+}
---
+++ src/test/java/com/spring/core/chap02/HotelTest.java
@@ -0,0 +1,18 @@
+package com.spring.core.chap02;
+
+import com.spring.core.chap02.config.HotelManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+class HotelTest {
+    // 스프링 컨테이너를 읽어오는 객체
+    AnnotationConfigApplicationContext ctx
+            = new AnnotationConfigApplicationContext(HotelManager.class);
+
+    @Test
+    void hotelAutoTest() {
+        Hotel hotel = ctx.getBean(Hotel.class);
+        hotel.reserve();
+    }
+
+}
